22f7282303b893c4ed80e31b6dd00aa80514df93
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-08-05 19:40:38 +0200

    init project and create a generators:arrow-functional-s and arrow-functional-m

---
+++ mern-starter/server/server.js
@@ -0,0 +1,175 @@
+import Express from 'express';
+import compression from 'compression';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import path from 'path';
+import IntlWrapper from '../client/modules/Intl/IntlWrapper';
+
+// Initialize the Express App
+//inicjalizacja aplikacji Expres
+const app = new Express();
+
+// Set Development modes checks
+const isDevMode = process.env.NODE_ENV === 'development' || false;
+const isProdMode = process.env.NODE_ENV === 'production' || false;
+
+// Run Webpack dev server in development mode
+if (isDevMode) {
+  // Webpack Requirements
+  // eslint-disable-next-line global-require
+  //zaleznosci webpaca
+  const webpack = require('webpack');
+  // eslint-disable-next-line global-require
+  const config = require('../webpack.config.dev');
+  // eslint-disable-next-line global-require
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  // eslint-disable-next-line global-require
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    watchOptions: {
+      poll: 1000,
+    },
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+// React And Redux Setup
+import { configureStore } from '../client/store';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import Helmet from 'react-helmet';
+
+// Import required modules
+import routes from '../client/routes';
+import { fetchComponentData } from './util/fetchData';
+import posts from './routes/post.routes';
+import dummyData from './dummyData';
+import serverConfig from './config';
+
+// Set native promises as mongoose promise
+mongoose.Promise = global.Promise;
+
+// MongoDB Connection
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(serverConfig.mongoURL, (error) => {
+    if (error) {
+      console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+      throw error;
+    }
+
+    // feed some dummy data in DB.
+    dummyData();
+  });
+}
+
+// Apply body Parser and server public assets and routes
+app.use(compression());
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: false }));
+app.use(Express.static(path.resolve(__dirname, '../dist/client')));
+app.use('/api', posts);
+
+// Render Initial HTML
+const renderFullPage = (html, initialState) => {
+  const head = Helmet.rewind();
+
+  // Import Manifests
+  const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
+  const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
+
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        ${head.base.toString()}
+        ${head.title.toString()}
+        ${head.meta.toString()}
+        ${head.link.toString()}
+        ${head.script.toString()}
+
+        ${isProdMode ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
+        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+      </head>
+      <body>
+        <div id="root">${process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`}</div>
+        <script>
+          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+          ${isProdMode ?
+          `//<![CDATA[
+          window.webpackManifest = ${JSON.stringify(chunkManifest)};
+          //]]>` : ''}
+        </script>
+        <script src='${isProdMode ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
+        <script src='${isProdMode ? assetsManifest['/app.js'] : '/app.js'}'></script>
+      </body>
+    </html>
+  `;
+};
+
+const renderError = err => {
+  const softTab = '&#32;&#32;&#32;&#32;';
+  const errTrace = isProdMode ?
+    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
+  return renderFullPage(`Server Error${errTrace}`, {});
+};
+
+// Server Side Rendering based on routes matched by React-router.
+// korzystamy z middleware'a, który przepuszcza każde zapytanie przez funkcję match
+app.use((req, res, next) => {
+  // funkcja match stara się dopasować na podstawie url z którego zostało wykonane zapytanie do jednego z routów
+  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+  // jeśli podczas dopasowywania routingu pojawił się błąd należy odesłać odpowiedź błędu razem z kodem 500 oznaczającym błąd serwera
+    if (err) {
+      return res.status(500).end(renderError(err));
+    }
+// w przypadku obecności obiektu Location, zostaniemy przekierowani na tę lokalizację. 302 to kod, który oznacza właśnie przekierowanie
+    if (redirectLocation) {
+      return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    }
+// renderProps to obiekt, który powinniśmy przekazać jako kontekst dla React Routera. Jeśli go nie ma możemy przepuścić middleware'a dalej
+    if (!renderProps) {
+      return next();
+    }
+
+    const store = configureStore();
+// Pobieramy dane potrzebne do "nawodnienia" komponentu.
+    return fetchComponentData(store, renderProps.components, renderProps.params)
+// jeśli nam to wyjdzie korzystamy z metody renderToString zawartej w paczce react-dom/server i na podstawie przekazanych
+//komponentów generujemy kod HTML, który następnie wrzucimy do pełnego szablonu HTML naszej strony.
+      .then(() => {
+        const initialView = renderToString(
+          <Provider store={store}>
+            <IntlWrapper>
+              <RouterContext {...renderProps} />
+            </IntlWrapper>
+          </Provider>
+        );
+// ustalamy ostateczny stan na podstawie wygenerowanego komponentu tak, aby przy starcie aplikacji po stronie
+//klienta można było zasilić store jakimiś danymi.
+        const finalState = store.getState();
+
+        res
+          .set('Content-Type', 'text/html')
+          .status(200)
+            // renderujemy całą stronę na podstawie stworzonego pierwszego widoku i stanu apki
+          .end(renderFullPage(initialView, finalState));
+      })
+      //// wyłapujemy wszystkie błędy związane z pobieraniem danych do komponentu
+      .catch((error) => next(error));
+  });
+});
+
+// start app
+app.listen(serverConfig.port, (error) => {
+  if (!error) {
+    console.log(`MERN is running on port: ${serverConfig.port}! Build something amazing!`); // eslint-disable-line
+  }
+});
+
+export default app;
