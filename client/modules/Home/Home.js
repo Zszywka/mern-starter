9b8968b9b389176e845cd1efd30517adf1543590
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-08-06 19:05:29 +0200

    Change the page:home

--- client/modules/Home/Home.js
+++ client/modules/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
 
 // Import Style
 import styles from './Home.css';
@@ -9,25 +10,18 @@ import styles from './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>Hello world!</h2>
+      <div className={styles.jumbotron + ' ' + "jumbotron"}>
+        <div className="container text-center">
+          <h1 className="display-4 mb-4">Hello, world!</h1>
+          <hr className="my-4 mt-4" />
+          <p>Click on button below if You want to see posts list.</p>
+          <p className="lead">
+            <Link className="btn btn-primary btn-lg mt-4" to={'/'} href="#" role="button">Learn more</Link>
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
-
-Home.propTypes = {
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default Home;
